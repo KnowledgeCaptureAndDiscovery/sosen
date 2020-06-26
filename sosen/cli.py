25d40cb78d8e32aa76602d2f1b8b1832bea0aebe
Aidan Kelley <aidanmkelley@gmail.com>
2020-06-26 17:32:06 -0500

    Added an end-to-end test. Will need to change the input data once the scraping finishes in order to better represent the process

---
+++ sosen/cli.py
@@ -0,0 +1,106 @@
+import requests
+import json
+from somef import cli as somef_cli
+
+from .get_zenodo_query_data import get_zenodo_query_data
+from .data_to_graph import DataGraph
+from .get_data import get_zenodo_data
+import math
+import os
+from rdflib import Graph
+import rdflib
+from SPARQLWrapper import SPARQLWrapper
+
+
+def run(queries, all, graph_out, zenodo_data, threshold, format, data_dict):
+    print("running")
+    if not zenodo_data:
+        if all:
+            queries = {""}
+        else:
+            with open(queries, "r") as in_handle:
+                # get all of the queries
+                queries = {stripped_line for stripped_line in
+                           (line.rstrip("\n") for line in in_handle)
+                           # if len(stripped_line) > 0
+                           }
+
+        print(queries)
+        # get the data from zenodo for each
+        data_and_urls = (get_zenodo_data(query) for query in queries)
+
+        # flatten it all into one object and use a dict to guarantee uniqueness
+        data_and_urls_flattened = {}
+        total_len = 0
+        for dict_obj in data_and_urls:
+            total_len += len(dict_obj)
+            data_and_urls_flattened.update(dict_obj)
+            print(f"total len: {len(data_and_urls_flattened)}, total gathered: {total_len}, repeats: {total_len - len(data_and_urls_flattened)}")
+
+    else:
+        with open(zenodo_data, "r") as in_file:
+            data_and_urls_flattened = json.load(in_file)
+
+
+    # save the data and urls flattened object
+    with open(graph_out + ".data_and_urls.json", "w") as out_file:
+        json.dump(data_and_urls_flattened, out_file)
+
+    # make sure that the github urls are all unique, too
+    github_urls = {data["github_url"] for data in data_and_urls_flattened.values()}
+
+    cli_data = {}
+
+    if data_dict is not None and os.path.exists(data_dict):
+        with open(data_dict, "r") as json_in:
+            cli_data = json.load(json_in)
+
+        print(f"found {data_dict} with {len(cli_data)} entries")
+
+    # get the data from the cli
+    index = 0
+    for github_url in github_urls:
+        if github_url not in cli_data:
+            index += 1
+            data = somef_cli.cli_get_data(threshold, repo_url=github_url)
+            cli_data.update({github_url: data})
+
+            # save every 10 times... saving is pretty quick.
+            if index == 10:
+                index = 0
+                with open(data_dict, "w") as json_out:
+                    json.dump(cli_data, json_out)
+                print(f"saved cli_data to {data_dict}")
+        else:
+            print(f"{github_url} found cached in {data_dict}")
+
+    # save at the end, too
+    with open(data_dict, "w") as json_out:
+        json.dump(cli_data, json_out)
+
+    graph = DataGraph()
+
+    filtered_data = (data for data in data_and_urls_flattened.values()
+                     if data["github_url"] in cli_data and cli_data[data["github_url"]] is not None)
+
+    for data in filtered_data:
+        graph.add_somef_data({**cli_data[data["github_url"]], "zenodo_data": data["zenodo_data"]})
+
+    with open(graph_out, "wb") as out_file:
+        out_file.write(graph.g.serialize(format=format))
+
+def get_data(queries, output, graph_in):
+    sparql = SPARQLWrapper(graph_in)
+
+    with open(queries, "r") as query_file:
+        print("loading queries")
+        queries_set = {stripped_line for stripped_line in
+                   (line.rstrip("\n") for line in query_file)
+                   if len(stripped_line) > 0
+                   }
+        print("querying API")
+        output_data = {query: get_zenodo_query_data(query, sparql) for query in queries_set}
+
+    print("saving data")
+    with open(output, "w") as out_file:
+        json.dump(output_data, out_file)
---
+++ test/end_to_end/test_end_to_end.py
@@ -0,0 +1,48 @@
+import unittest
+from sosen.cli import run
+import os
+
+# run the whole process on a file and verify the output is as expected
+
+
+class BaseEndToEnd(unittest.TestCase):
+    maxDiff = None
+
+    def setUp(self):
+        pass
+
+
+class FromCached(BaseEndToEnd):
+
+    def testEndToEndFromCached(self):
+        abs_path = os.path.abspath(os.path.dirname(__file__))
+        print(abs_path)
+
+        queries = None
+        all = None
+        zenodo_data = os.path.join(abs_path, "test_data_and_urls.json")
+        data_dict = os.path.join(abs_path, "test_data_dict.json")
+        graph_out = os.path.join(abs_path, "test_actual_out.ttl")
+        expected_out_file = os.path.join(abs_path, "test_expected_out.ttl")
+        threshold = 0.8
+        format = "turtle"
+
+        run(
+            queries=queries,
+            all=all,
+            zenodo_data=zenodo_data,
+            data_dict=data_dict,
+            graph_out=graph_out,
+            threshold=threshold,
+            format=format
+        )
+
+        with open(graph_out, "r") as actual_in:
+            actual_out = actual_in.read()
+
+        with open(expected_out_file, "r") as expected_in:
+            expected_out = expected_in.read()
+
+        self.assertEqual(actual_out, expected_out)
+
+
