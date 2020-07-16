68d2da9fa4f805b232c98d18c21b25c29aee6a42
Aidan Kelley <aidanmkelley@gmail.com>
2020-07-16 16:22:59 -0500

    changed sosen URI. Will need to be updated in the queries, too, once the KG is regenerated

--- sosen/schema/schema_prefixes.py
+++ sosen/schema/schema_prefixes.py
@@ -4,5 +4,5 @@ schema =  "https://schema.org/"
 sd = "https://w3id.org/okn/o/sd#"
 xsd = str(XSD)
 obj = "https://w3id.org/okn/o/i/"
-sosen = "http://example.org/sosen#"
+sosen = "https://w3id.org/okn/o/sosen#"
 rdfs = str(RDFS)
