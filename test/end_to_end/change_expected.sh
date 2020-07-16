436b943c0109e2aa1dd9478dc5056b2ed9deb8f3
Aidan Kelley <aidanmkelley@gmail.com>
2020-07-16 14:58:35 -0500

    updated tests for previous commit

---
+++ test/end_to_end/change_expected.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv test_actual_out.ttl test_expected_out.ttl
+mv test_actual_keyword_out.ttl test_expected_keyword_out.ttl
+mv test_long_actual_out.ttl test_long_expected_out.ttl
+mv test_long_actual_keyword_out.ttl test_long_expected_keyword_out.ttl
